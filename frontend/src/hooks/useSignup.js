ca0388951f1ef2cf3f8d067e6c649f19b3281f21
asif <asifmuhammedn7@gmail.com>
2024-03-20 18:34:59 +0530

    signup functionality

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,15 +1,19 @@
 import { useState } from "react"
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 
 const useSignup = () => {
   const [loading,setLoading] = useState(false);
-
+  const {setAuthUser} = useAuthContext()
   const signup = async ({fullName,username,password,confirmPassword,gender})=>{
    const success =  handleInputErrors({fullName,username,password,confirmPassword,gender});
+   console.log(success)
    if(!success) return;
+   toast.success("success")
    setLoading(true)
    try {
+    console.log('getting ')
     const res = await fetch("/api/auth/signup",{
         method:"POST",
         headers:{"Content-type":"application/json"},
@@ -18,6 +22,12 @@ const useSignup = () => {
 
     const data = await res.json();
     console.log(data)
+    if(data.error){
+        throw new Error(data.error)
+    }
+
+    localStorage.setItem("chatUser",JSON.stringify(data))
+    setAuthUser(data)
    } catch (error) {
         toast.error(error.message)
    } finally{
@@ -39,7 +49,8 @@ function handleInputErrors ({fullName,username,password,confirmPassword,gender})
         return false;
     }
     if(password.length<6){
-        toast.error('passwprd must be at least 6 characters');
+        toast.error('password must be at least 6 characters');
         return false;
     }
+    return true
 }
